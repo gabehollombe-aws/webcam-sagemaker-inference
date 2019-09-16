3ce9718c33d1aff5f27e7fe30d0ff7b6e7b01a4a
Gabe Hollombe <gabe@avantbard.com>
2019-09-16 16:56:56 +0800

    update lambda src for more recent aws-serverless-express

--- lambda/app.js
+++ lambda/app.js
@@ -4,6 +4,7 @@ Paste this into the app.js that Amplify opens for you when creating a new REST e
 
 var express = require('express')
 var bodyParser = require('body-parser')
+var awsServerlessExpress = require('aws-serverless-express')
 var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 const AWS = require('aws-sdk');
 
@@ -13,6 +14,8 @@ const AWS = require('aws-sdk');
 var app = express()
 app.use(bodyParser.json({limit: '10mb'}));
 app.use(awsServerlessExpressMiddleware.eventContext())
+
+const server = awsServerlessExpress.createServer(app)
 
 // Enable CORS for all methods
 app.use(function(req, res, next) {
@@ -26,21 +29,18 @@ app.post('/classify', async function(req, res) {
     apiVersion: '2017-05-13',
     region:req.body.endpointRegion,
   });
+
   const result = await sagemaker.invokeEndpoint({
     Body: Buffer.from(req.body.base64Image, 'base64'),
     EndpointName: req.body.endpointName,
     ContentType: 'application/x-image',
     Accept: 'application/json',
   }).promise();
-  
+
   res.json({predictions: JSON.parse(result.Body.toString())})
 });
 
 app.listen(3000, function() {
-    console.log("App started")
 });
 
-// Export the app object. When executing the application local this does nothing. However,
-// to port it to AWS Lambda we will create a wrapper around that will load the app from
-// this file
-module.exports = app
+exports.handler = (event, context) => { awsServerlessExpress.proxy(server, event, context) }
